a47ca8b4d527216f8b5788882269e173d87642a7
Xavier Sala <utrescu@gmail.com>
2018-08-22 20:09:22 +0200

    Initial commit

---
+++ KafkaConsumer/KafkaConsumer.cs
@@ -0,0 +1,73 @@
+﻿namespace KafkaConsumer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading;
+    using Confluent.Kafka;
+    using Confluent.Kafka.Serialization;
+
+    public class Program
+    {
+
+        static void processaMissatge(string message)
+        {
+            int dots = message.Split('.').Length - 1;
+            Thread.Sleep(dots * 1000);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Enter client id as param");
+                return;
+            }
+
+            var clientid = args[0];
+
+            var config = new Dictionary<string, object>
+            {
+                { "client.id", clientid },
+                { "group.id", "consumergroup" },
+                { "bootstrap.servers", "localhost:9092" },
+                { "enable.auto.commit", "false"}
+            };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.Subscribe(new string[] { "hello-topic" });
+
+                consumer.OnMessage += (_, msg) =>
+                {
+                    Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    // En el tutorial hi afegeixen Wait() però no sé si cal...
+                    processaMissatge(msg.Value);
+                    consumer.CommitAsync(msg).Wait();
+                };
+
+                consumer.OnError += (_, msg) =>
+                {
+                    Console.WriteLine($"Error {msg}");
+                };
+
+                consumer.OnPartitionEOF += (_, end)
+                    => Console.WriteLine($"Reached end of topic {end.Topic} partition {end.Partition}.");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cancelled = true;
+                };
+
+                Console.WriteLine("Ctrl-C per acabar.");
+
+                while (cancelled == false)
+                {
+                    consumer.Poll(TimeSpan.FromMilliseconds(100));
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/KafkaProducer.cs
@@ -0,0 +1,51 @@
+﻿namespace KafkaProducer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Text;
+    using Confluent.Kafka;
+    using Confluent.Kafka.Serialization;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var producerid = Dns.GetHostName();
+
+            var config = new Dictionary<string, object>
+            {
+                { "client.id", producerid },
+                { "bootstrap.servers", "localhost:9092" },
+                // Màxima garantia que no es perd cap missatge
+                { "default.topic.config", new Dictionary<string, object>
+                    {
+                        { "acks", "all" }
+                    }
+                }
+            };
+
+            var fitxer = "missatges.txt";
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                string text = "";
+
+                System.IO.StreamReader file = new System.IO.StreamReader(fitxer);
+                while ((text = file.ReadLine()) != null)
+                {
+                    // Sembla que m'ho podria estalviar amb .GetAwaiter().GetResult();
+                    // Posant-hi al darrere .Result són missatges síncrons
+                    System.Console.WriteLine($"Enviant: {text}");
+                    var deliveryReportTask = producer.ProduceAsync("hello-topic", null, text);
+                    deliveryReportTask.ContinueWith(task =>
+                    {
+                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+                    });
+                }
+                file.Close();
+                producer.Flush(-1);
+            }
+        }
+    }
+}
